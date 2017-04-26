c016f4699c62a301b64c369181ebc772a9d2120e
Conner Johnson <Conner.johnson33@gmail.com>
2017-04-26 13:02:59 -0400

    house full crud, CREATE bug for character

--- app/controllers/characters_controller.rb
+++ app/controllers/characters_controller.rb
@@ -8,6 +8,39 @@ def show
   @character = Character.find(params[:id])
 end
 
+def new
+  @character = Character.new
+end
+
+def create
+  @house = House.find(params[:id])
+  @character = @house.characters.create (character_params)
+  redirect_to @character
+end
+
+def edit
+  @house = House.find(params[:house_id])
+  @character = Character.find(params[:id])
+end
+
+def update
+  @house = House.find(params[:id])
+  @character = @house.characters.find(params[:id])
+  @character.update(character_params)
+  redirect_to @character
+end
+
+def destroy
+  @character = Character.find(params[:id])
+  @character.destroy
+  redirect_to characters_path
+end
+
+private
+
+def character_params
+  params.require(:character).permit(:name, :img_url, :house_id)
+end
 
 
 
